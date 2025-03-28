7f421fe430f46ee522d865678c42893c1c367111
Júlia Sena <juliasena19@gmail.com>
2025-03-27 22:23:41 -0300

    desktop version ok, responsiveness and animations under construction

---
+++ js/configs.js
@@ -0,0 +1,75 @@
+var typed = new Typed("#animated-text", {
+  strings: ["Desenvolvedora Front-end", "Data Science Student"],
+  typeSpeed: 80,
+  backSpeed: 50,
+  loop: true,
+});
+
+ScrollReveal().reveal(".reveal", {
+  duration: 1200,
+  origin: "bottom",
+  distance: "50px",
+  easing: "ease-in-out",
+  reset: true,
+});
+
+ScrollReveal().reveal(".reveal-left", {
+  origin: "left",
+  distance: "100px",
+  duration: 1000,
+});
+ScrollReveal().reveal(".reveal-right", {
+  origin: "right",
+  distance: "100px",
+  duration: 1000,
+});
+
+particlesJS.load("particles-js", "js/particlesjs-config.json", function () {});
+
+function scrollParaConteudo() {
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: "smooth",
+  });
+}
+
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const navItems = document.querySelectorAll(".menu-item");
+
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= 100 && rect.bottom >= 100) {
+      navItems.forEach((item) => {
+        item.classList.remove("active");
+        if (item.getAttribute("href") === `#${section.id}`) {
+          item.classList.add("active");
+        }
+      });
+    }
+  });
+});
+
+document.querySelectorAll(".menu-item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault(); // Impede o comportamento padrão do link
+
+    const targetId = item.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    const navbarHeight = document.querySelector(".navbar").offsetHeight;
+
+    window.scrollTo({
+      top: targetSection.offsetTop - navbarHeight - 10, // Ajuste para evitar corte do título
+      behavior: "smooth",
+    });
+  });
+});
+
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
